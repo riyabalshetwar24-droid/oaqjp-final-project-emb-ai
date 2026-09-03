55605c6673b0fe0c86bdfce1e8b2a74c63227c7f
Riya Balshetwar <riya.balshetwar24@vit.edu>
2026-09-03 01:26:46 -0400

    Fix emotion detection unit tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,9 +1,21 @@
-from emotion_detection import emotion_detector
+from .emotion_detection import emotion_detector
 
 
-def test_emotion_detector():
+def test_joy():
     assert emotion_detector("I am glad this happened")["dominant_emotion"] == "joy"
+
+
+def test_anger():
     assert emotion_detector("I am really mad about this")["dominant_emotion"] == "anger"
+
+
+def test_disgust():
     assert emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"] == "disgust"
+
+
+def test_fear():
     assert emotion_detector("I am so frightened that I could not move")["dominant_emotion"] == "fear"
+
+
+def test_sadness():
     assert emotion_detector("I am so sad about this")["dominant_emotion"] == "sadness"
